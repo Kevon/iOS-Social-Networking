2f8dba47df482613f4601032913d36961f8bad5c
Kevin Skompinski <kevons5252@gmail.com>
2016-05-15 21:14:42 -0400

    Initial Commit

---
+++ Social Networking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Social Networking
+//
+//  Created by Kevin on 5/15/16.
+//  Copyright © 2016 Kevin Skompinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
